3d35703336f237d46bbe8c782960aa005a98c06e
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-21 16:05:38 -0500

    handlebars implement for views

--- src/app.js
+++ src/app.js
@@ -2,7 +2,30 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Coz'
+  });
+});
+
+app.get('/help', (req, res) => { 
+  res.render('help', {
+    title: 'Help',
+    message: 'Sample help message'
+  });
+});
+
+app.get('', (req, res) => { 
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Coz'
+  });
+});
 
 app.get('/', (req, res) => { 
   res.send('<h1>Hello express</h1>');
