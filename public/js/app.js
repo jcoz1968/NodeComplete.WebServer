372d15a9ca2d3c3b539a87a852584afc2d1e4677
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-21 19:09:38 -0500

    creating a search form for address

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,24 @@
 console.log('js is loaded');
 
-fetch('http://localhost:3000/weather?address=checotah').then((response) => { 
-  response.json().then((data) => { 
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => { 
+  e.preventDefault();
+  const location = search.value;
+  if (location === '') {
+    
+  }
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => { 
+    response.json().then((data) => { 
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });  
+  console.log(location);
 });
+
